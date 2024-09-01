b9be04542c641bbe01fd18c10caeeb8f5ae93b62
OlegBezverkhy <124727891+OlegBezverkhy@users.noreply.github.com>
2024-09-01 14:23:35 +0300

    убрал лишнее из __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
 
@@ -10,7 +9,6 @@ app.config.from_object('config.Config')
 
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
-bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
